0710de60ad3c86981e00a51cafe39c4157783f72
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2023-03-02 14:05:18 -0300

    Update Updates.js

--- screens/Updates.js
+++ screens/Updates.js
@@ -1,17 +1,3 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+// faça os importes necessários
 
-export default class UpdateScreen extends Component {
-    render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                <Text>Tela de Atualizações!</Text>
-            </View>
-        )
-    }
-}
+// crie a classe UpdateScreen
